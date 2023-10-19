bd625ff395e0a8ec48c774231ca142184a4700a0
raviejay <puspusraviejay@gmail.com>
2023-10-19 17:16:43 +0800

    Added update functionalities

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -43,9 +43,14 @@ class CarouselItemsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CarouselItemsRequest $request, string $id)
     {
-        //
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::findOrFail($id);
+
+        $carouselItem->update($validated);
+        return $carouselItem;
     }
 
     /**
