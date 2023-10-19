20092aeeae0ca79a1732c9d042cb5799f0cb6e91
raviejay <puspusraviejay@gmail.com>
2023-10-19 15:57:12 +0800

    Added show functionalities

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -30,7 +30,8 @@ class CarouselItemsController extends Controller
      */
     public function show(string $id)
     {
-        //
+       return CarouselItems::findOrFail($id);
+
     }
 
     /**
