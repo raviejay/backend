e1ce735d92eaea601340a4f18c632fea090ebc0d
raviejay <puspusraviejay@gmail.com>
2023-10-19 17:06:45 +0800

    Added store functionalities and validation

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\CarouselItems;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\CarouselItemsRequest;
 
 class CarouselItemsController extends Controller
 {
@@ -20,9 +21,14 @@ class CarouselItemsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CarouselItemsRequest $request)
     {
-        //
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::create($validated);
+
+        return $carouselItem;
     }
 
     /**
@@ -50,7 +56,7 @@ class CarouselItemsController extends Controller
         $carouselItem = CarouselItems::findOrFail($id);
 
         $carouselItem->delete();
-        
+
         return $carouselItem;
     }
 }
